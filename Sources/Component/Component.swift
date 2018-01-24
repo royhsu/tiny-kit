08b07970f78ec18911d46819736561f9ebe23891
Roy Hsu <roy.hsu@tinyworld.cc>
2018-01-24 17:15:44 +0800

    add component

---
+++ Sources/Component/Component.swift
@@ -0,0 +1,11 @@
+//
+//  Component.swift
+//  TinyKit
+//
+//  Created by Roy Hsu on 24/01/2018.
+//  Copyright © 2018 TinyWorld. All rights reserved.
+//
+
+// MARK: - Component
+
+public typealias Component = ComponentNode & ViewRenderable & ViewRender
---
+++ Sources/View/ViewRender.swift
@@ -0,0 +1,15 @@
+//
+//  ViewRender.swift
+//  TinyKit
+//
+//  Created by Roy Hsu on 24/01/2018.
+//  Copyright © 2018 TinyWorld. All rights reserved.
+//
+
+// MARK: - ViewRender
+
+public protocol ViewRender {
+    
+    func render(_ renderable: ViewRenderable)
+    
+}
