72cbd4629bd7c138e33a1ed0ea39f3fa3bcc92fd
Roy Hsu <roy.hsu@tinyworld.cc>
2018-02-08 20:52:41 +0800

    fix documentation for view renderer

--- Sources/View/ViewRender.swift
+++ Sources/View/ViewRender.swift
@@ -10,17 +10,11 @@
 
 import TinyCore
 
+/// A view render should define its own way to renderables.
+/// It can also ignore the preferred content size of renderables if it makes sense.
 public protocol ViewRender {
     
     var renderables: AnyCollection<ViewRenderable> { get }
-    
-    /**
-    A render should define its way to render objects.
-    
-     - Parameter renderables: The objects to be rendered.
-    
-     Note: View Render can ignore the preferred content size of renderables if it's reasonable.
-    */
     
     func render() -> Promise<Void>
     
