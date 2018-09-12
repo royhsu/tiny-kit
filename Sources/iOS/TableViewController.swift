6328db6d6d911d9a9694e7312164692a1f1e2ee8
Roy Hsu <roy.hsu@tinyworld.cc>
2018-09-12 23:29:00 +0800

    add generic table view controller.

---
+++ Sources/iOS/TableViewController.swift
@@ -0,0 +1,85 @@
+//
+//  TableViewController.swift
+//  TinyKit
+//
+//  Created by Roy Hsu on 2018/9/12.
+//  Copyright © 2018 TinyWorld. All rights reserved.
+//
+
+// MARK: - TableViewController
+
+import UIKit
+import TinyCore
+
+open class TableViewController<S: Storage>: UIViewController where S.Key == Int {
+    
+    private typealias Index = Int
+    
+    private final let tableView = UITableView()
+    
+    private final let dataSourceController = UITableViewDataSourceController()
+    
+    public final var storage: S? {
+        
+        didSet {
+            
+            guard
+                let storage = storage
+                else { return }
+            
+            let subscription = storage.keyDiff.subscribe { _ in
+                
+                DispatchQueue.main.async {
+                    
+                    self.tableView.reloadData()
+                    
+                }
+            }
+            
+            subscriptions = [ subscription ]
+            
+        }
+        
+    }
+    
+    private final var subscriptions: [ObservableSubscription] = []
+    
+    open override func loadView() { view = tableView }
+    
+    open override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        tableView.dataSource = dataSourceController
+        
+        dataSourceController.setNumberOfSections { [weak self] _ in
+            
+            guard
+                let maxKey = self?.storage?.maxKey
+                else { return 0 }
+            
+            return maxKey + 1
+            
+        }
+        
+        dataSourceController.setNumberOfRows { _, _ in
+            
+            return 1
+            
+        }
+        
+        dataSourceController.setCellForRow { [weak self] _, indexPath in
+            
+            let cell = UITableViewCell()
+            
+            let element = self?.storage?[indexPath.section]
+            
+            cell.textLabel?.text = "\(indexPath)"
+            
+            return cell
+            
+        }
+        
+    }
+    
+}
