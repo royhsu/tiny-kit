58073419289860f2ce0da1d6c73243ffc181a4fc
Roy Hsu <roy.hsu@tinyworld.cc>
2018-09-24 21:54:05 +0800

    apply array to section collection.

---
+++ Sources/Template/Array+SectionCollection.swift
@@ -0,0 +1,15 @@
+//
+//  Array+SectionCollection.swift
+//  TinyKit
+//
+//  Created by Roy Hsu on 2018/9/24.
+//  Copyright © 2018 TinyWorld. All rights reserved.
+//
+
+// MARK: - SectionCollection
+
+extension Array: SectionCollection where Element == Template {
+    
+    public func section(at index: Int) -> Section { return self[index] }
+    
+}
