16b7524ed6fa9518af651690ab0055389f424db6
Roy Hsu <roy.hsu@tinyworld.cc>
2018-11-04 10:35:28 +0800

    add edge constraints.

--- Sources/iOS/View/UIView+Subview.swift
+++ Sources/iOS/View/UIView+Subview.swift
@@ -8,77 +8,44 @@
 
 // MARK: - Subview
 
-import UIKit
-
 // Reference: https://stackoverflow.com/questions/26652854/ios8-cell-constraints-break-when-adding-disclosure-indicator
 
-// swiftlint:disable large_tuple
 public extension UIView {
-
-    /// A convenient method to add subview and pin it to edges to the parent with Auto Layout.
-    @available(*, deprecated: 1.0, message: "Please use wrapSubview(:within:constraintBuilder:) instead.")
-    @discardableResult
-    public final func wrapSubview(_ view: UIView) -> (
-        topConstraint: NSLayoutConstraint,
-        leadingConstraint: NSLayoutConstraint,
-        bottomConstraint: NSLayoutConstraint,
-        trailingConstraint: NSLayoutConstraint
-    ) {
-
-        view.removeFromSuperview()
-
-        view.translatesAutoresizingMaskIntoConstraints = false
-
-        let topConstraint = topAnchor.constraint(equalTo: view.topAnchor)
-
-        let leadingConstraint = leadingAnchor.constraint(equalTo: view.leadingAnchor)
-
-        let bottomConstraint = bottomAnchor.constraint(equalTo: view.bottomAnchor)
-
-        bottomConstraint.priority = UILayoutPriority(900.0)
-
-        let trailingConstraint = trailingAnchor.constraint(equalTo: view.trailingAnchor)
-
-        trailingConstraint.priority = UILayoutPriority(900.0)
-
-        addSubview(view)
-
-        NSLayoutConstraint.activate(
-            [
-                topConstraint,
-                leadingConstraint,
-                bottomConstraint,
-                trailingConstraint
-            ]
-        )
-
-        return (
-            topConstraint,
-            leadingConstraint,
-            bottomConstraint,
-            trailingConstraint
-        )
-
+    
+    public struct EdgeConstraints {
+        
+        public let topConstraint: NSLayoutConstraint
+        
+        public let leadingConstraint: NSLayoutConstraint
+        
+        public let bottomConstraint: NSLayoutConstraint
+        
+        public let trailingConstraint: NSLayoutConstraint
+        
+        public init(
+            top topConstraint: NSLayoutConstraint,
+            leading leadingConstraint: NSLayoutConstraint,
+            bottom bottomConstraint: NSLayoutConstraint,
+            trailing trailingConstraint: NSLayoutConstraint
+        ) {
+            
+            self.topConstraint = topConstraint
+            
+            self.leadingConstraint = leadingConstraint
+            
+            self.bottomConstraint = bottomConstraint
+            
+            self.trailingConstraint = trailingConstraint
+            
+        }
+        
     }
-    
-}
-// swiftlint:enable large_tuple
-    
-public extension UIView {
     
     /// A convenient method to add subview and pin its edges to the parent view.
     public final func wrapSubview(
         _ view: UIView,
         within guideKeyPath: KeyPath<UIView, UILayoutGuide>? = nil,
-        constraintBuilder: (
-            (
-                _ topConstraint: NSLayoutConstraint,
-                _ leadingConstraint: NSLayoutConstraint,
-                _ bottomConstraint: NSLayoutConstraint,
-                _ trailingConstraint: NSLayoutConstraint
-            )
-            -> Void
-        )? = nil
+        constraintBuilder: ( (EdgeConstraints) -> Void )? = nil
     ) {
         
         view.removeFromSuperview()
@@ -133,10 +100,12 @@ public extension UIView {
         trailingConstraint.priority = UILayoutPriority(900.0)
         
         constraintBuilder?(
-            topConstraint,
-            leadingConstraint,
-            bottomConstraint,
-            trailingConstraint
+            EdgeConstraints(
+                top: topConstraint,
+                leading: leadingConstraint,
+                bottom: bottomConstraint,
+                trailing: trailingConstraint
+            )
         )
         
         NSLayoutConstraint.activate(
