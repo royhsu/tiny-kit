8180127a1c780f0335b8660752e23338b9cc1ee0
Roy Hsu <roy.hsu@tinyworld.cc>
2019-01-27 13:04:41 +0800

    make view controller conform to initializable.

---
+++ Sources/View/ViewController+Initializable.swift
@@ -0,0 +1,11 @@
+//
+//  ViewController+Initializable.swift
+//  TinyKit
+//
+//  Created by Roy Hsu on 2019/1/27.
+//  Copyright © 2019 TinyWorld. All rights reserved.
+//
+
+// MARK: - Initializable
+
+extension ViewController: Initializable { }
